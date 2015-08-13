5b40abde9f70b616ac6644c1b41f0491cec1f50d
Vildar Khisyametdinov <vildar82@gmail.com>
2015-08-13 09:23:11 +0300

    first

---
+++ TamplateLayerFilter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("TamplateLayerFilter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Импорт фильтра слоев из шаблона")]
+[assembly: AssemblyProduct("TamplateLayerFilter")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("b3582b01-d4d6-451e-acb2-1b4d99597c48")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TamplateLayerFilter/TemplateLayerFilter.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//AUTOCAD
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.LayerManager;
+using Microsoft.Win32;
+
+namespace Vil.Acad.TemplateLayerFilter
+{
+   public static class Commands
+   {
+      [CommandMethod("TemplateLayerFilter")]
+      public static void TemplateLayerFilter()
+      {
+         Document doc = Application.DocumentManager.MdiActiveDocument;
+         Database db = doc.Database;
+         Editor ed = doc.Editor;
+
+         string qNewTemplateFile = GetQNewTemplateFile();
+         if (qNewTemplateFile == string.Empty)
+         {
+            ed.WriteMessage("Не определен шаблон по умолчанию, из которого должны корпироваться фильтры слоев.");
+            return;
+         }
+
+         LayerFilterTree lft = db.LayerFilters;
+         ImportLayerFilterTree(qNewTemplateFile, lft.Root, db);
+         db.LayerFilters = lft;
+      }
+
+      private static string GetQNewTemplateFile()
+      {
+         string res = string.Empty;
+         RegistryKey regKey = Registry.CurrentUser.OpenSubKey(HostApplicationServices.Current.UserRegistryProductRootKey);
+         regKey = regKey.OpenSubKey("Profiles");
+         regKey = regKey.OpenSubKey(regKey.GetValue(null).ToString());
+         regKey = regKey.OpenSubKey("General");
+         res = regKey.GetValue("QnewTemplate").ToString();
+         return res;
+      }
+
+      private static void ImportLayerFilterTree(string filePath, LayerFilter lfDest, Database dbDest)
+      {
+         using (Database dbSrc = new Database(false, false))
+         {
+            dbSrc.ReadDwgFile(filePath, FileOpenMode.OpenForReadAndAllShare, false, string.Empty);
+            ImportNestedFilters(dbSrc.LayerFilters.Root, lfDest,dbSrc, dbDest );
+         }
+      }
+
+      public static void ImportNestedFilters(LayerFilter srcFilter, LayerFilter destFilter, Database srcDb, Database destDb)
+      {
+         using (Transaction t = srcDb.TransactionManager.StartTransaction())
+         {
+            LayerTable lt = t.GetObject(srcDb.LayerTableId, OpenMode.ForRead, false) as LayerTable;
+
+            foreach (LayerFilter sf in srcFilter.NestedFilters)
+            {
+               // Получаем слои, которые следует клонировать в db
+               // Только те, которые участвуют в фильтре
+               ObjectIdCollection layerIds = new ObjectIdCollection();
+               foreach (ObjectId layerId in lt)
+               {
+                  LayerTableRecord ltr = t.GetObject(layerId, OpenMode.ForRead, false) as LayerTableRecord;
+                  if (sf.Filter(ltr))
+                  {
+                     layerIds.Add(layerId);
+                  }
+               }
+
+               // Клонируем слои во внешнюю db 
+               IdMapping idmap = new IdMapping();
+               if (layerIds.Count > 0)
+               {
+                  srcDb.WblockCloneObjects(layerIds, destDb.LayerTableId, idmap, DuplicateRecordCloning.Replace, false);
+               }
+
+               // Опеределяем не было ли фильтра слоев
+               // с таким же именем во внешней db
+               LayerFilter df = null;
+               foreach (LayerFilter f in destFilter.NestedFilters)
+               {
+                  if (f.Name.Equals(sf.Name))
+                  {
+                     df = f;
+                     break;
+                  }
+               }
+
+               if (df == null)
+               {
+                  if (sf is LayerGroup)
+                  {
+                     // Создаем новую группу слоев если
+                     // ничего не найдено
+                     LayerGroup sfgroup = sf as LayerGroup;
+                     LayerGroup dfgroup = new LayerGroup();
+                     dfgroup.Name = sf.Name;
+
+                     df = dfgroup;
+
+                     LayerCollection lyrs = sfgroup.LayerIds;
+                     foreach (ObjectId lid in lyrs)
+                     {
+                        if (idmap.Contains(lid))
+                        {
+                           IdPair idp = idmap[lid];
+                           dfgroup.LayerIds.Add(idp.Value);
+                        }
+                     }
+                     destFilter.NestedFilters.Add(df);
+                  }
+                  else
+                  {
+                     // Создаем фильтр слоев если
+                     // ничего не найдено
+                     df = new LayerFilter();
+                     df.Name = sf.Name;
+                     df.FilterExpression = sf.FilterExpression;
+                     destFilter.NestedFilters.Add(df);
+                  }
+               }
+
+               // Импортируем другие фильтры
+               ImportNestedFilters(sf, df, srcDb, destDb);
+            }
+            t.Commit();
+         }
+      }
+   }
+}
